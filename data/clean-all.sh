20582cbc12f520cc18c5b965490bdade35b241a8
Anastasia <anastasi.bi@foxmail.com>
2025-11-01 14:43:34 +0800

    add clean-all

---
+++ data/clean-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+rm results_release.csv.gz || true
+rm -r tectonic || true
+rm -r breakdown-stats/ || true
+rm breakdowns.tar.gz || true
+rm storage_0.1.tar.gz || true
+# end
